a2a48cf9f92bca1e036179a34b9006c21e42decc
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-28 15:27:42 +0530

    1 file modified

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ array1.forEach(element => console.log(element));
 let birds = ['crow','parrot','sparrow'];
 birds.sort();
 console.log(birds);
+
+// find
+let num = [9,27,8,19,4];
+let result = num.find((x) => x > 20);
+console.log(result);
