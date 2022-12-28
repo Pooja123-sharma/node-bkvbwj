0851abb3187cce10ad6762a9c9383f14e7b48062
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-28 15:24:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // foreach
 let array1 = ['1','2','3','4','5'];
 array1.forEach(element => console.log(element));
+
+// sort
+let birds = ['crow','parrot','sparrow'];
+birds.sort();
+console.log(birds);
